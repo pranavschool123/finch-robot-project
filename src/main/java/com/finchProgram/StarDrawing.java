ce6092c38f7b8162ddb526a944059d706071aed7
pranavschool123 <pkunderu0001@mymail.lausd.net>
2025-06-04 10:00:09 -0700

    updated Code

--- src/main/java/com/finchProgram/StarDrawing.java
+++ src/main/java/com/finchProgram/StarDrawing.java
@@ -3,22 +3,12 @@ package com.finchProgram;
 import com.birdbrain.Finch;
 
 public class StarDrawing {
-    
-    public static final int MOVE_DURATION = 800;
-    public static final int TURN_DURATION = 440;
-    public static final int MOTOR_SPEED = 50;
-
     public static void main(String[] args) {
 
         Finch bird = new Finch();
-        
-        for (int i=0; i < 5; i++) {
-            drawLine(bird);
-            turnStarAngle(bird);
-        }
-        
+        bird.setMove("F", 10, 50);
 
-        // bird.stop();
+        
         bird.disconnect();
     }
 }
