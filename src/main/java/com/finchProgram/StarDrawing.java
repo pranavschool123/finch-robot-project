335881bbcafb3648acdfc1e9559fbfe2a70e8748
pranavschool123 <pkunderu0001@mymail.lausd.net>
2025-06-04 09:15:07 -0700

    starting the finch

---
+++ src/main/java/com/finchProgram/StarDrawing.java
@@ -0,0 +1,10 @@
+public class StarDrawing {
+    public static void main(String[] args) {
+
+        Finch bird = new Finch();
+        bird.setMove("F", 10, 50);
+        bird.disconnect
+
+
+    }
+}
