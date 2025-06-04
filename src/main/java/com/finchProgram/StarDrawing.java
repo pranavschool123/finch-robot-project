0a6809f8a6b78405d48d7c21cb2ac160c8a18576
pranavschool123 <pkunderu0001@mymail.lausd.net>
2025-06-04 09:55:44 -0700

    finch

--- src/main/java/com/finchProgram/StarDrawing.java
+++ src/main/java/com/finchProgram/StarDrawing.java
@@ -3,6 +3,11 @@ package com.finchProgram;
 import com.birdbrain.Finch;
 
 public class StarDrawing {
+    
+    public static final int MOVE_DURATION = 800;
+    public static final int TURN_DURATION = 440;
+    public static final int MOTOR_SPEED = 50;
+
     public static void main(String[] args) {
 
         Finch bird = new Finch();
