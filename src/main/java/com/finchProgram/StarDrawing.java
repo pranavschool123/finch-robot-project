48d5e72a54863ab58d67aee4ecca06a801261c8c
pranavschool123 <pkunderu0001@mymail.lausd.net>
2025-06-04 14:36:22 -0700

    finished code adding comments

--- src/main/java/com/finchProgram/StarDrawing.java
+++ src/main/java/com/finchProgram/StarDrawing.java
@@ -7,20 +7,28 @@ public class StarDrawing {
 
         Finch bird = new Finch();
 
-        double arr[] = [22.5, 140, 150, 145, 150];
+        double[] arr = {22.5, 140, 150, 145, 150};
 
+        for (double angle: arr) {
+        bird.setTurn("R", angle, 50);
+        bird.setMove("F", 10, 50);
+        bird.setBeak(50, 100, 80);
+        bird.pause(1);
+        bird.setBeak(0, 0, 0);
+        
 
+        }
 
-        bird.setTurn("R", 22.5, 50);
-        bird.setMove("F", 10, 50);
-        bird.setTurn("R", 140, 50);
-        bird.setMove("F", 10, 50);
-        bird.setTurn("R", 150, 50);
-        bird.setMove("F", 10, 50);
-        bird.setTurn("R", 145, 50);
-        bird.setMove("F", 10, 50);
-        bird.setTurn("R", 150, 50);
-        bird.setMove("F", 11, 50);
+        // bird.setTurn("R", 22.5, 50);
+        // bird.setMove("F", 10, 50);
+        // bird.setTurn("R", 140, 50);
+        // bird.setMove("F", 10, 50);
+        // bird.setTurn("R", 150, 50);
+        // bird.setMove("F", 10, 50);
+        // bird.setTurn("R", 145, 50);
+        // bird.setMove("F", 10, 50);
+        // bird.setTurn("R", 150, 50);
+        // bird.setMove("F", 11, 50);
         
 
         
