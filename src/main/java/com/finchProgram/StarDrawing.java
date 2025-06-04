4eb8c2e69affd50e5148ecf79df0825e10a694a8
pranavschool123 <pkunderu0001@mymail.lausd.net>
2025-06-04 13:23:23 -0700

    array Creation

--- src/main/java/com/finchProgram/StarDrawing.java
+++ src/main/java/com/finchProgram/StarDrawing.java
@@ -6,7 +6,22 @@ public class StarDrawing {
     public static void main(String[] args) {
 
         Finch bird = new Finch();
+
+        double arr[] = [22.5, 140, 150, 145, 150];
+
+
+
+        bird.setTurn("R", 22.5, 50);
         bird.setMove("F", 10, 50);
+        bird.setTurn("R", 140, 50);
+        bird.setMove("F", 10, 50);
+        bird.setTurn("R", 150, 50);
+        bird.setMove("F", 10, 50);
+        bird.setTurn("R", 145, 50);
+        bird.setMove("F", 10, 50);
+        bird.setTurn("R", 150, 50);
+        bird.setMove("F", 11, 50);
+        
 
         
         bird.disconnect();
