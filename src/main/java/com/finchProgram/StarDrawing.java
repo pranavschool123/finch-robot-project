845e3eca1e80171d5a6bb252de45109afa80999c
pranavschool123 <pkunderu0001@mymail.lausd.net>
2025-06-05 05:46:08 -0700

    Updated Comments

--- src/main/java/com/finchProgram/StarDrawing.java
+++ src/main/java/com/finchProgram/StarDrawing.java
@@ -2,13 +2,31 @@ package com.finchProgram;
 
 import com.birdbrain.Finch;
 
+/**
+ * The StarDrawing program uses the Finch robot to draw a pattern resembling
+ * resembling a 5-pointed star. The program is able to do this by using 
+ * both turning and forward movement commands. Each turn is marked by a
+ * specific angle, stored in an array.
+ * 
+ * This program also allows the LED on the beak of the robot to light up.
+ * It does this at every point on the star.
+ */
 public class StarDrawing {
+    
+    /**
+     * This is the main method that executes the star drawing program sequence
+     * utilizing the Finch robot.
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
 
+        // Creates the new Finch robot object
         Finch bird = new Finch();
 
+        // Array that stores the turning angles used during the program
         double[] arr = {22.5, 140, 150, 145, 150};
 
+        // Loop through each angle to draw the star pattern and light up the beak at the points
         for (double angle: arr) {
         bird.setTurn("R", angle, 50);
         bird.setMove("F", 10, 50);
@@ -18,20 +36,10 @@ public class StarDrawing {
         
 
         }
-
-        // bird.setTurn("R", 22.5, 50);
-        // bird.setMove("F", 10, 50);
-        // bird.setTurn("R", 140, 50);
-        // bird.setMove("F", 10, 50);
-        // bird.setTurn("R", 150, 50);
-        // bird.setMove("F", 10, 50);
-        // bird.setTurn("R", 145, 50);
-        // bird.setMove("F", 10, 50);
-        // bird.setTurn("R", 150, 50);
-        // bird.setMove("F", 11, 50);
         
 
         
+        // Disconnects the Finch when star drawing sequence finishes
         bird.disconnect();
     }
 }
